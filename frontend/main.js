535046f4dd8b315c1991b8e0e6a25f5a9e9b7773
Moisés Corrêa Gomes <54242235+skittlexyz@users.noreply.github.com>
2024-10-14 09:07:30 -0300

    Made checkboxes work and upgraded refresh button.

--- frontend/main.js
+++ frontend/main.js
@@ -11,6 +11,8 @@ const configureButton = $("#configure-button");
 const startDate = $("#start-date");
 const endDate = $("#end-date");
 
+const dateOrderButton = $("#date-order-button");
+
 const addButton = $("#add-button");
 const deleteButton = $("#delete-button");
 const editButton = $("#edit-button");
@@ -23,22 +25,26 @@ const spentSpan = $("#spent-span");
 const profitSpan = $("#profit-span");
 const savingsSpan = $("#savings-span");
 
+const allLinesCheckbox = $("#all-lines-checkbox");
+
 const errorModal = $("#error-modal");
 const errorModalLabel = $("#error-modal-label");
 const errorModalText = $("#error-modal-text");
 
-let data;
+const summary = $("#summary");
+
+let data = [];
 
 const tableLineModel = "" +
-"<tr id='<LINE-ID>'>" +
+    "<tr id='<LINE-ID>'>" +
     "<td>" +
-        "<input id='<LINE-CHECKBOX>' class='form-check-input' type='checkbox'>" +
+    "<input id='<LINE-CHECKBOX>' class='form-check-input' type='checkbox'>" +
     "</td>" +
     "<th scope='row' class='tw-text-center'><ID></th>" +
     "<td style='color:<VALUE-COLOR>;' class='tw-font-bold'><VALUE></td>" +
     "<td class=''><DATE></td>" +
     "<td class=''><DESCRIPTION></td>" +
-"</tr>";
+    "</tr>";
 
 (async () => {
     try {
@@ -56,35 +62,80 @@ const tableLineModel = "" +
     }
 })();
 
+function displayData() {
+    $("tbody").html("");
+    for (let i = 0; i < data.length; i++) {
+        $(`#line-checkbox-${i}`).off();
+        $("tbody").html(
+            $("tbody").html() +
+            tableLineModel
+                .replace("<LINE-ID>", `line-${i}`)
+                .replace("<LINE-CHECKBOX>", `line-checkbox-${i}`)
+                .replace("<ID>", `${data[i].id}`)
+                .replace("<VALUE-COLOR>", (data[i].value > 0 ? "#198754" : "#DC3545"))
+                .replace("<VALUE>", (data[i].value > 0 ? `<i class="bi bi-arrow-down"></i>&nbsp;${BRL.format(data[i].value).replace("-", "")}` : `<i class="bi bi-arrow-up"></i>&nbsp;${BRL.format(data[i].value).replace("-", "")}`))
+                .replace("<DATE>", `${data[i].date.replace("-", "/").replace("-", "/")}`)
+                .replace("<DESCRIPTION>", `${data[i].description}`)
+        );
+    };
+}
+
 async function fetchData() {
     await fetch(apiUrl + "investments")
         .then(async (response) => {
             const responseBody = await response.json();
             // console.log(responseBody);
             data = responseBody;
-        }).then(() => {
-            $("tbody").html("");
-            for (let i = 0; i < data.length; i++) {
-                $(`#line-${i}`).off();
-                $("tbody").html(
-                    $("tbody").html() +
-                    tableLineModel
-                        .replace("<LINE-ID>", `line-${i}`)
-                        .replace("<LINE-CHECKBOX>", `line-checkbox-${i}`)
-                        .replace("<ID>", `${data[i].id}`)
-                        .replace("<VALUE-COLOR>", (data[i].value > 0 ? "#198754" : "#DC3545"))
-                        .replace("<VALUE>", (data[i].value > 0 ? `<i class="bi bi-arrow-down"></i>&nbsp;${BRL.format(data[i].value).replace("-", "")}` : `<i class="bi bi-arrow-up"></i>&nbsp;${BRL.format(data[i].value).replace("-", "")}`))
-                        .replace("<DATE>", `${data[i].date.replace("-", "/").replace("-", "/")}`)
-                        .replace("<DESCRIPTION>", `${data[i].description}`)
-                )
-                $(`#line-checkbox-${i}`).on("click", () => {
-                    console.log(1);
-                    $(`#line-${i} > *`).each(() => {
-                        $(this).toggleClass("selected-line");
-                    });
-                })
-            }
-        });
+        }).then(() => displayData()).then(() => main());
 }
 
-refreshButton.on("click", fetchData);
+function main() {
+    refreshButton.on("click", () => location.reload() );
+    collapseExpandButton.on("click", () => {
+        $("#collapse-expand-button").toggleClass("tw-rotate-180")
+        $("body > main > section > hr").toggle(250);
+        summary.toggle(250);
+    })
+    allLinesCheckbox.on("click", function () {
+        if ($(this).is(':checked')) {
+            for (let i = 0; i < data.length; i++) {
+                $(`#line-${i} > *`).each(function () {
+                    $(this).addClass("selected-line");
+                });
+                $(`#line-${i} > td >input`).prop("checked", true);
+            }
+        } else {
+            for (let i = 0; i < data.length; i++) {
+                $(`#line-${i} > *`).each(function () {
+                    $(this).removeClass("selected-line");
+                });
+                $(`#line-${i} > td > input`).prop("checked", false);
+            }
+        }
+    });
+
+    for (let i = 0; i < data.length; i++) {
+        $(`#line-checkbox-${i}`).on("click", function () {
+            if ($(this).is(':checked')) {
+                console.log(1)
+                $(`#line-${i} > *`).each(function () {
+                    $(this).addClass("selected-line");
+                });
+            } else {
+                console.log(2)
+                $(`#line-${i} > *`).each(function () {
+                    $(this).removeClass("selected-line");
+                });
+            }
+        });
+    };
+    dateOrderButton.on("click", function () {
+        $(this).toggleClass("bi-arrow-down");
+        $(this).toggleClass("bi-arrow-up");
+        if (allLinesCheckbox.is(':checked')) {
+            allLinesCheckbox.prop("checked", false);
+        };
+        data = data.reverse();
+        displayData();
+    });
+}
